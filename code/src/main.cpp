3cda05921445410b399c30a3bf2450dde6642005
OnlineCop <onlinecop@gmail.com>
2020-04-13 07:34:00 -0600

    Adds rudimentary command-line parser.
    -h: show the help
    -t: run the tests
    -d [#]: set minimum number of digits to '#'
    -f: open a file to read in from (not implemented)
    -n [#]: provide a number on the command-line to use

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -2,6 +2,7 @@
 #include "../include/maxproduct.h"
 #include "../include/testing.h"
 
+#include <iostream>
 #include <string>
 
 namespace {
@@ -29,31 +30,113 @@ const std::string hpcString{
     "74982810508277521659834594761360129982400036745363"
 };
 
+struct ParseInputs
+{
+    bool runTests{ false };
+    bool showHelp{ false };
+    size_t numDigitsToParse{ 4 };
+    std::pair<bool, std::string> parseExternalFile{};
+    std::pair<bool, std::string> userSuppliedNumber{};
+};
+
+ParseInputs doParse(int argc, char** argv)
+{
+    ParseInputs parseInputs{};
+
+    for (int arg = 0; arg < argc; ++arg)
+    {
+        std::string argvString = argv[arg];
+        if (argvString == "-t" || argvString == "--test")
+        {
+            parseInputs.runTests = true;
+            continue;
+        }
+
+        if (argvString == "-h" || argvString == "--help")
+        {
+            parseInputs.showHelp = true;
+        }
+
+        if (argvString == "-d" && arg + 1 < argc)
+        {
+            parseInputs.numDigitsToParse = strtoul(argv[arg + 1], nullptr, 0);
+            if ((parseInputs.numDigitsToParse == LLONG_MAX || parseInputs.numDigitsToParse == LLONG_MIN) &&
+                errno == ERANGE)
+            {
+                // Invalid number; ignore it.
+                parseInputs.numDigitsToParse = 4;
+            }
+            ++arg;
+            continue;
+        }
+
+        if (argvString == "-f" && arg + 1 < argc)
+        {
+            parseInputs.parseExternalFile = std::make_pair(true, argv[arg + 1]);
+            ++arg;
+            continue;
+        }
+
+        if (argvString == "-n" && arg + 1 < argc)
+        {
+            // User will provide number on command line.
+            parseInputs.userSuppliedNumber = std::make_pair(true, argv[arg + 1]);
+            ++arg;
+            continue;
+        }
+    }
+
+    return parseInputs;
+}
+
 } // anonymous namespace
 
-int main()
+int main(int argc, char** argv)
 {
-    // For debugging only.
-    //Debugging::Testing::runTests();
+    ParseInputs parseInputs = doParse(argc, argv);
+
+    if (parseInputs.runTests)
+    {
+        Debugging::Testing::runTests();
+        return 0;
+    }
+
+    if (parseInputs.showHelp)
+    {
+        std::cout << "Sorry; the help file isn't ready yet. Try '-f [filename]' or '-d 8' for now." << std::endl;
+        return 0;
+    }
+
+    if (parseInputs.parseExternalFile.first == true)
+    {
+        // TODO: Read number in from a file.
+
+        // STUB
+        std::string fileContents = "1234005567091110999901111";
+    }
+
+    std::string stringToParse{ hpcString };
+    if (parseInputs.userSuppliedNumber.first == true)
+    {
+        stringToParse = parseInputs.userSuppliedNumber.second;
+    }
 
 
     // Step 1: Split the number into runs NOT containing 0's, since we know their product will equal 0.
-    constexpr size_t runLength = 8;
+    std::vector<Hpc::NonZeroRun> nonZeroRun = Hpc::getNonZeroRuns(stringToParse, parseInputs.numDigitsToParse);
 
-    std::vector<Hpc::NonZeroRun> nonZeroRun = Hpc::getNonZeroRuns(hpcString, runLength);
-
-    Hpc::MaxProduct maxProduct(hpcString, runLength);
+    Hpc::MaxProduct maxProduct(stringToParse, parseInputs.numDigitsToParse);
 
 
-    // Step 2: For each of the runs returned, scan [runLength] digits at a time and calculate their product.
+    // Step 2: For each of the runs returned, scan [numDigitsToParse] digits at a time and calculate their product.
     for (Hpc::NonZeroRun run : nonZeroRun)
     {
         maxProduct.calculateRun(run);
     }
 
 
-    // Step 3: Display the [runLength] digits that produced the largest product.
-    constexpr bool showOriginalNumber{ false };
+    // Step 3: Display the [numDigitsToParse] digits that produced the largest product.
+    constexpr bool showOriginalNumber{ true };
     Debugging::displayFound(maxProduct, showOriginalNumber);
 
     return 0;
